fc6e15f04118da320fc13d52ccdfa6ed806e6210
superstar1205 <devking1205@gmail.com>
2020-08-17 20:58:59 +0530

    navbar redesigned

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,88 @@
 <template>
   <div>
-      <nav class="bg-white px-2 border-gray-100 border-b-2">
-        <div class="container mx-auto flex flex-col justify-center md:flex-row md:justify-between md:px-8">
-          <div class="flex justify-center items-center">
-           <nuxt-link to="/" class="py-3">Logo</nuxt-link>
-          </div>
-           <div class="-mb-px flex menu justify-between px-3">
-            <nuxt-link to="/" class="no-underline text-teal-dark border-b-2 border-blue-700 uppercase tracking-wide font-bold text-xs py-4 mr-8">
-                Home
-            </nuxt-link>
-            <nuxt-link class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-4 mr-8" to="/">
-                Products
-            </nuxt-link>
-            <nuxt-link class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-4 mr-8" to="/">
-                Discounts
-            </nuxt-link>
-            <nuxt-link class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-4" to="/">
-                Customers
-            </nuxt-link>
-           </div>
-        </div>
-    </nav>
+    <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2">
+      <div class="flex-1 flex justify-between items-center">
+        <a href="#">
+          <svg width="32" height="36" viewBox="0 0 32 36" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M15.922 35.798c-.946 0-1.852-.228-2.549-.638l-10.825-6.379c-1.428-.843-2.549-2.82-2.549-4.501v-12.762c0-1.681 1.12-3.663 2.549-4.501l10.825-6.379c.696-.41 1.602-.638 2.549-.638.946 0 1.852.228 2.549.638l10.825 6.379c1.428.843 2.549 2.82 2.549 4.501v12.762c0 1.681-1.12 3.663-2.549 4.501l-10.825 6.379c-.696.41-1.602.638-2.549.638zm0-33.474c-.545 0-1.058.118-1.406.323l-10.825 6.383c-.737.433-1.406 1.617-1.406 2.488v12.762c0 .866.67 2.05 1.406 2.488l10.825 6.379c.348.205.862.323 1.406.323.545 0 1.058-.118 1.406-.323l10.825-6.383c.737-.433 1.406-1.617 1.406-2.488v-12.757c0-.866-.67-2.05-1.406-2.488l-10.825-6.379c-.348-.21-.862-.328-1.406-.328zM26.024 13.104l-7.205 13.258-3.053-5.777-3.071 5.777-7.187-13.258h4.343l2.803 5.189 3.107-5.832 3.089 5.832 2.821-5.189h4.352z"
+            />
+          </svg>
+        </a>
+      </div>
+
+      <label for="menu-toggle" class="pointer-cursor lg:hidden block">
+        <svg
+          @click="setOpen"
+          class="fill-current text-gray-900"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+        >
+          <title>menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </label>
+      <!-- <input class="hidden" type="checkbox" id="menu-toggle" /> -->
+
+      <div :class="open? 'hidden': 'block'" class="lg:flex lg:items-center lg:w-auto w-full" id="menu">
+        <nav>
+          <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+            <li>
+              <nuxt-link to="/"
+                class="lg:p-4 py-3 px-2 rounded md:rounded-none  md:px-0 block border-b-2 border-transparent hover:bg-gray-100 mb-2 md:mb-0 md:hover:border-indigo-400"
+              >Categories</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/"
+                class="lg:p-4 py-3 px-2 rounded md:rounded-none md:px-0 block border-b-2 border-transparent hover:bg-gray-100 mb-2 md:mb-0 md:hover:border-indigo-400"
+              >About</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/"
+                class="lg:p-4 py-3 px-2 rounded md:rounded-none md:px-0 block border-b-2 border-transparent hover:bg-gray-100 mb-2 md:mb-0 md:hover:border-indigo-400"
+              >Support</nuxt-link>
+            </li>
+            
+          </ul>
+        </nav>
+        <a href="#" class="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
+          <img
+            class="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400"
+            src="https://avatars2.githubusercontent.com/u/34214453?s=460&u=cb1ed0948a1229eca70d749ef99cbe304c189110&v=4"
+            alt="Manas Paul"
+          />
+        </a>
+      </div>
+    </header>
     <Nuxt />
   </div>
 </template>
 
 <script>
 export default {
-  transition: 'page'
-}
+  transition: "page",
+  data(){
+    return {
+      open : false
+    }
+  },
+  methods : {
+    setOpen(){
+      this.open = !this.open
+    }
+  }
+};
 </script>
 
 <style>
-.page-enter-active, .page-leave-active {
-  transition: opacity .5s;
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
 }
-.page-enter, .page-leave-to {
+.page-enter,
+.page-leave-to {
   opacity: 0;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div >
-    <div class="featured px-3 bg-blue-100">
+    <div class="featured px-3 py-2 bg-blue-100">
      <div class="container h-auto mx-auto px-3 py-10 flex flex-col" v-for="content in featured" :key="content.slug">
         <p class="ml-3 text-center px-3 inline-block w-24 rounded bg-blue-200">Featured</p>
         <nuxt-link :to="content.slug">
           <h1 class="px-3 text-3xl font-semibold">{{content.title}}</h1>
+        <p class="mt-4 text-xl text-gray-900 px-3">
+          {{ content.description }}
+        </p>
         </nuxt-link>
      </div>
     </div>
-    <div class="mx-auto container px-3">
+    <div class="mx-auto container px-3 mt-8">
       <h1 class="text-4xl font-semibold py-5 px-3">Latest Article</h1>
       <hr class="px-3">
       <ul class="py-5">
@@ -17,6 +20,9 @@
             <div>
               <h2 class="text-xl text-black font-semibold">{{ article.title }}</h2>
             </div>
+            <p class="mt-4 text-xl text-gray-900">
+              {{ article.description }}
+            </p>
           </NuxtLink>
         </li>
       </ul>
@@ -28,12 +34,12 @@
   export default {
     async asyncData({ $content, params }) {
       const articles = await $content(params.slug)
-        .only(['title', 'slug'])
+        .only(['title', 'slug', 'description'])
         .where({ featured: { $eq: true } })
         .sortBy('createdAt', 'asc')
         .fetch()
       const featured = await $content(params.slug)
-        .only(['title', 'slug'])
+        .only(['title', 'slug', 'description'])
         .where({ featured: { $eq: false } })
         .sortBy('createdAt', 'asc')
         .fetch()
